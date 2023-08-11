e7d12b670a1edf3d9269c054b23cd6169f2b9b60
Ryan <iperish.x@gmail.com>
2023-08-11 03:09:30 +0200

    implement new font settings< update login buttons

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-    import axios from "axios";
-    import Fa from "svelte-fa";
-    import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-	import GoogleLoginButton from "$components/GoogleLoginButton.svelte";
-	import TwitchLoginButton from "$components/TwitchLoginButton.svelte";
+    import { faGoogle, faTwitch } from "@fortawesome/free-brands-svg-icons";
+    import LoginButton from "$components/LoginButton.svelte";
+	import { GoogleOAuthUrlHref, TwitchOAuthUrlHref } from "$lib/AuthUrlHelper";
 </script>
 
-<div class={"flex flex-col w-72 mx-auto rounded-md shadow-lg my-auto bg-base-300 py-5 gap-y-5"}>
-    <div class={"lobster text-2xl font-bold text-center"}>
+<div class={"flex flex-col w-72 mx-auto rounded-[2.5rem] shadow-lg my-auto bg-base-300 py-5 gap-y-5"}>
+    <div class={"select-none font-lobster text-2xl font-bold text-center"}>
         My<span class={"text-indigo-400"}>Vods</span>
     </div>
-    <GoogleLoginButton />
-    <TwitchLoginButton />
-    
+    <LoginButton href={GoogleOAuthUrlHref} icon={faGoogle} provider={"Google"} />
+    <LoginButton href={TwitchOAuthUrlHref} icon={faTwitch} provider={"Twitch"} />
 </div>
