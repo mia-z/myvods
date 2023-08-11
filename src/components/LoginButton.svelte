e0de1a048434e09ab5ce9bf46ed6b6bfa55ad5ae
Ryan <iperish.x@gmail.com>
2023-08-11 03:07:11 +0200

    refactor login buttons to single component

---
+++ src/components/LoginButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Fa from "svelte-fa";
+    import type { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+
+    export let href: string;
+    export let icon: IconDefinition;
+    export let provider: "Twitch" | "Google"
+</script>
+
+<a {href} class={`btn btn-neutral w-60 rounded-full shadow-lg mx-auto flex flex-row`}>
+    <div class={"text-xl"}>
+        <Fa class={"my-auto"} {icon} />
+    </div>
+    <div class={"my-auto"}>
+        Log in with {provider}
+    </div>
+</a>
