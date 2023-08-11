5421e440a030021603456224dd6b6f24e2ca3111
Ryan <iperish.x@gmail.com>
2023-08-11 03:08:02 +0200

    update authurl helper functions

--- src/lib/AuthUrlHelper.ts
+++ src/lib/AuthUrlHelper.ts
@@ -1,15 +1,42 @@
-import { PUBLIC_YOUTUBE_CLIENT_ID, PUBLIC_YOUTUBE_AUTH_CALLBACK_URI } from "$env/static/public";
+import { PUBLIC_GOOGLE_CLIENT_ID, PUBLIC_GOOGLE_AUTH_CALLBACK_URI, PUBLIC_TWITCH_CLIENT_ID, PUBLIC_TWITCH_AUTH_CALLBACK_URI } from "$env/static/public";
 
 const GoogleOAuthUrl = (): URL => {
-    const loginUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
-    loginUrl.searchParams.append("client_id", PUBLIC_YOUTUBE_CLIENT_ID);
-    loginUrl.searchParams.append("redirect_uri", PUBLIC_YOUTUBE_AUTH_CALLBACK_URI);
-    loginUrl.searchParams.append("response_type", "token");
-    loginUrl.searchParams.append("scope", "https://www.googleapis.com/auth/youtube.readonly");
+    const scopes = [
+        "https://www.googleapis.com/auth/youtube.readonly",
+        "https://www.googleapis.com/auth/userinfo.profile"
+    ];
 
-    return loginUrl;
+    const url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    url.searchParams.append("scope", scopes.join(" "));
+    url.searchParams.append("access_type", "offline");
+    url.searchParams.append("include_granted_scopes", "true");
+    url.searchParams.append("response_type", "code");
+    url.searchParams.append("redirect_uri", PUBLIC_GOOGLE_AUTH_CALLBACK_URI);
+    url.searchParams.append("client_id", PUBLIC_GOOGLE_CLIENT_ID);
+    return url;
 }
 
+const GoogleOAuthUrlHref = GoogleOAuthUrl().href;
+
+const TwitchOAuthUrl = (): URL => {
+    const scopes = [
+        ""
+    ];
+
+    const url = new URL("https://id.twitch.tv/oauth2/authorize");
+    url.searchParams.append("scope", scopes.join(" "));
+    url.searchParams.append("response_type", "code");
+    url.searchParams.append("redirect_uri", PUBLIC_TWITCH_AUTH_CALLBACK_URI);
+    url.searchParams.append("client_id", PUBLIC_TWITCH_CLIENT_ID);
+
+    return url;
+}
+
+const TwitchOAuthUrlHref = TwitchOAuthUrl().href;
+
 export {
-    GoogleOAuthUrl
+    GoogleOAuthUrl,
+    GoogleOAuthUrlHref,
+    TwitchOAuthUrl,
+    TwitchOAuthUrlHref
 }
