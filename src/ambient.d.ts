6c01314d51c657b48c2244477696f6cb9c7c77a5
Ryan <iperish.x@gmail.com>
2023-08-11 03:07:42 +0200

    add twitch definintions

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -5,3 +5,24 @@ declare type YoutubeTokenRes = {
     scope: string,
     refresh_token?: string
 }
+
+type TwitchTokenRes = {
+    access_token: string,
+    expires_in: number,
+    token_type: "Bearer",
+    scope: string,
+    refresh_token: string
+}
+
+type TwitchValidateRes = {
+    client_id: string,
+    login: string,
+    scopes: string[],
+    user_id: string,
+    expires_in: number
+}
+
+type TwitchUser = {
+    login: string,
+    userId: string
+}
