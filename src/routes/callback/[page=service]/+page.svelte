e4e1805e72bc656df49f8707465ba57d27f4deef
Ryan <iperish.x@gmail.com>
2023-08-13 19:07:48 +0200

    update loading area for oauth login

--- src/routes/callback/[page=service]/+page.svelte
+++ src/routes/callback/[page=service]/+page.svelte
@@ -1,27 +1,62 @@
 <script lang="ts">
 	import { onMount } from "svelte";
     import { page } from "$app/stores";
+    import Fa from "svelte-fa";
+    import { faGoogle, faTwitch } from "@fortawesome/free-brands-svg-icons";
+    import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 	import { setupGoogleAuth } from "$lib/GoogleAuthHelper";
 	import { setupTwitchAuth } from "$lib/TwitchAuthHelper";
+	import { AxiosError } from "axios";
 
+    let service: string = $page.params.page;
+
+    let loginError: string | null = null;
 
     onMount(async () => {
-        const service = $page.params.page;
         const code = $page.url.searchParams.get("code");
         
         if (!code) {
             throw new Error("OAuth provider didnt return a code!");
         }
 
-        switch (service) {
-            case "google": await setupGoogleAuth(code); break;
-            case "twitch": await setupTwitchAuth(code); break;
-            default: throw new Error("Unsupported OAuth provider (This shouldnt ever happen lol)");
+        try {
+            switch (service) {
+                case "google": await setupGoogleAuth(code); break;
+                case "twitch": await setupTwitchAuth(code); break;
+                default: throw new Error("Unsupported OAuth provider (This shouldnt ever happen lol)");
+            }
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                console.log( e.response?.data)
+                loginError = e.response?.data?.message
+            } else {
+                loginError = "Unknown error!"
+            }
+            
         }
     });
 
 </script>
 
-<div>
-    redirecting
+<div class="min-h-screen w-full flex flex-col">
+    <div class={"flex flex-col w-96 mx-auto rounded-[2.5rem] shadow-lg my-auto bg-base-300 py-5"}>
+        <div class={"select-none font-lobster text-2xl mb-5 font-bold text-center"}>
+            My<span class={"text-indigo-400"}>Vods</span>
+        </div>
+        {#if loginError}
+            <div class={"mx-auto text-2xl font-bold text-red-400 mb-5"}>
+                Error!
+            </div>
+            <div class={"mx-auto px-10"}>
+                <div class={"border-2 p-4 border-gray-800 bg-base-200 text-sm font-roboto-monospace text-center font-bold"}>
+                    {loginError}
+                </div>
+            </div>
+        {:else}
+            <div class={"mx-auto text-xl"}>
+                Logging in via&nbsp;<span class={"font-bold capitalize"}>{service}</span>
+            </div>
+            <Fa icon={faSpinner} class={"text-2xl animate-spin"} />
+        {/if}
+    </div>
 </div>
