215cec320f4786dfab3c427b804dd5e4a4d761ea
Ryan <iperish.x@gmail.com>
2023-09-17 02:28:44 +0200

    vite redundancy removal

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,4 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import type { UserConfig } from "vite";
-
-const config: UserConfig = {
-	plugins: [sveltekit()],
-};
 
 export default {
 	plugins: [sveltekit()]
