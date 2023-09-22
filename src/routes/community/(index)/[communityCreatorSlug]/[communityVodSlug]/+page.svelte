d286dc69891382f2e255ae17e5e40673a69e541d
Ryan <iperish.x@gmail.com>
2023-09-22 20:53:26 +0200

    feat: annotation tools now grows and shrinks video rather than overflowing

--- src/routes/community/(index)/[communityCreatorSlug]/[communityVodSlug]/+page.svelte
+++ src/routes/community/(index)/[communityCreatorSlug]/[communityVodSlug]/+page.svelte
@@ -6,18 +6,18 @@
 	import YoutubePlayer from "$components/YoutubePlayer.svelte";
     import { Duration, DateTime } from "luxon";
 	import Sidebar from "$components/Sidebar.svelte";
-    import { faListOl, faPenToSquare, faTrash, faSave, faSpinner, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+    import { faListOl, faPenToSquare, faTrash, faSave, faSpinner, faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
     import Fa from "svelte-fa";
 	import NewAnnotationForm from "./NewAnnotationForm.svelte";
     import { error } from "$lib/toast";
 	import EditAnnotationForm from "./EditAnnotationForm.svelte";
-	import { fade } from "svelte/transition";
+	import { fade, slide } from "svelte/transition";
 	import { goto, invalidateAll } from "$app/navigation";
 
     export let data: PageData;
 
     let leftDrawerOpen: boolean = false;
-    let rightDrawerOpen: boolean = false;
+    let toolsDrawerOpen: boolean = true;
 
     let setTimestamp: ((seek: number) => Promise<void>);
     let videoDuration: number;
@@ -93,7 +93,7 @@
     </div>
 </Sidebar>
 
-{#if data.communityContributorNick}
+<!-- {#if data.communityContributorNick}
     <Sidebar side={"right"} width={"w-144"} bind:open={rightDrawerOpen}>
         <div class={"flex flex-col h-full overflow-y-auto styled-scrollbars"}>
             <div class={"w-full flex flex-col"}>
@@ -184,7 +184,7 @@
             </div>
         </div>
     </Sidebar>
-{/if}
+{/if} -->
 
 <div class={"w-screen h-screen relative"}>
     <div class={"absolute top-0 left-0 flex"}>
@@ -203,24 +203,126 @@
                     </button>            
                 </div>
             </div>
-            {#if data.communityContributorNick}
-                <div class={"button-tool-wrapper absolute top-[calc(50vh-32px)] left-[calc(100vw-6rem)]"}>
+        </div>
+    </div>
+    <div class={"flex flex-row h-full w-full overflow-x-hidden"}>
+        <div class={"flex-grow"}>
+            {#if data.communityVodWithAnnotations.service === "Twitch"}
+                <TwitchPlayer videoId={data.communityVodWithAnnotations.videoId} />
+            {/if}
+            {#if data.communityVodWithAnnotations.service === "Kick"}
+                <KickPlayer videoId={data.communityVodWithAnnotations.videoId} />
+            {/if}
+            {#if data.communityVodWithAnnotations.service === "Youtube"}
+                <YoutubePlayer bind:currentTime bind:duration={videoDuration} bind:seek={setTimestamp} videoId={data.communityVodWithAnnotations.videoId} />
+            {/if}
+        </div>
+        {#if data.communityContributorNick}
+            <div class={`transition-all tools-wrapper relative ${toolsDrawerOpen ? "w-144" : "w-0"}`}>
+                <div class={"absolute top-[calc(50%-40px)] -left-20"}>
                     <div class={"tooltip"} data-tip="Open Tools">
-                        <button on:click={() => { rightDrawerOpen = true }} class={"btn btn-neutral rounded-full btn-lg "}>
-                            <Fa icon={faPenToSquare} />
+                        <button on:click={() => { toolsDrawerOpen = !toolsDrawerOpen }} class={"btn btn-neutral rounded-full btn-lg "}>
+                            <div class={"swap"}>
+                                <input type="checkbox" bind:checked={toolsDrawerOpen} />
+                                <div class={"swap-on"}>
+                                    <Fa icon={faArrowRight} />
+                                </div>
+                                <div class={"swap-off"}>
+                                    <Fa icon={faArrowLeft} />
+                                </div>
+                            </div>
                         </button>        
                     </div>
                 </div>
-            {/if}
-        </div>
+                <div class={`w-144 flex flex-col h-full overflow-y-auto styled-scrollbars`}>
+                    <div class={"w-full flex flex-col"}>
+                        <NewAnnotationForm currentVodTime={currentTime} {data} />
+                    </div>
+                    <div class={"divider divider-vertical px-5"}></div>
+                    <div class={"text-center text-2xl"}>
+                        Annotations
+                    </div>
+                    <div class={"flex flex-col space-y-2 p-2"}>
+                        {#each data.communityVodWithAnnotations.annotations.sort((a, b) => {
+                            return a.timestamp - b.timestamp;
+                        }) as annotation (annotation.id)}
+                            {#if editingAnnotationId === annotation.id}
+                                <EditAnnotationForm bind:editingAnnotationId {data} {annotation} />
+                            {:else}
+                                <div class={"flex flex-row flex-wrap bg-base-300 p-2 gap-2 rounded-xl relative"}>
+                                    {#if deletingId === annotation.id}
+                                        <div transition:fade={{ duration: 150 }} class={"absolute top-0 left-0 w-full h-full backdrop-blur-sm rounded-2xl z-30 flex flex-col justify-center"}>
+                                            <div class={"flex bg-neutral flex-col mb-2 mx-5 p-2 rounded-xl"}>
+                                                <div class={"text-center text-lg text-white"}>
+                                                    Are you sure you want to delete annotation at timestamp:
+                                                </div>
+                                                <div class={"text-center text-lg text-blue-400 underline"}>
+                                                    {Duration.fromMillis(annotation.timestamp).toFormat("hh:mm:ss")}
+                                                </div>
+                                            </div>
+                                            {#if (isDeleting && annotation.id === deletingId)}
+                                                <Fa class={"animate-spin text-3xl"} icon={faSpinner}/>
+                                            {:else}
+                                                <div class={"flex flex-row space-x-2 justify-center"}>
+                                                    <button on:click={() => onDeleteConfirmClick(annotation.id)} class={"btn btn-error w-1/3"}>
+                                                        Delete
+                                                    </button>
+                                                    <button on:click={() => deletingId = -1} class={"btn btn-warning w-1/3"}>
+                                                        Cancel
+                                                    </button>
+                                                </div>
+                                            {/if}
+                                        </div>
+                                    {/if}
+                                    <div class={"w-full flex flex-row pr-2"}>
+                                        <div class={"w-1/2 bg-base-200 rounded-xl text-sm p-1 pl-2 my-auto"}>
+                                            <span class={"text-white font-bold"}>Timestamp:</span>&nbsp;{Duration.fromMillis(annotation.timestamp).toFormat("hh:mm:ss")}
+                                        </div>
+                                        <div class={"ml-auto pt-1 text-2xl hover:text-warning active:text-yellow-300 hover:cursor-pointer"}>
+                                            <div class={"tooltip"} data-tip={"Edit"}>
+                                                <button on:click={() => editingAnnotationId = annotation.id}>
+                                                    <Fa icon={faPenToSquare} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class={"ml-3 pt-1 text-2xl hover:text-error active:text-red-300 hover:cursor-pointer"}>
+                                            <div class={"tooltip"} data-tip={"Delete"}>
+                                                <button on:click={() => deletingId = annotation.id}>
+                                                    <Fa icon={faTrash} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class={"flex-1 flex flex-col"}>
+                                        <div class={"label-text font-bold pt-1 text-white bg-base-200 rounded-t-xl w-fit px-5 rounded-b-none"}>
+                                            Game
+                                        </div>
+                                        <div class={"p-1 pl-2 text-center bg-base-200 rounded-xl rounded-tl-none text-sm"}>
+                                            {annotation.game || "Nothing"}
+                                        </div>
+                                    </div>
+                                    <div class={"flex-1 flex flex-col"}>
+                                        <div class={"label-text font-bold pt-1 text-white bg-base-200 rounded-t-xl w-fit px-5 rounded-b-none"}>
+                                            Subject
+                                        </div>
+                                        <div class={"p-1 pl-2 text-center bg-base-200 rounded-xl rounded-tl-none text-sm"}>
+                                            {annotation.subject || "Memes"}
+                                        </div>
+                                    </div>
+                                    <div class={"w-full flex flex-col"}>
+                                        <div class={"label-text font-bold pt-1 text-white bg-base-200 rounded-t-xl w-fit px-5 rounded-b-none"}>
+                                            Note
+                                        </div>
+                                        <div class={"p-1 pl-2 bg-base-200 rounded-xl rounded-tl-none text-sm line-clamp-2"}>
+                                            {annotation.note}
+                                        </div>
+                                    </div>
+                                </div>
+                            {/if}
+                        {/each}
+                    </div>
+                </div>
+            </div>
+        {/if}
     </div>
-    {#if data.communityVodWithAnnotations.service === "Twitch"}
-        <TwitchPlayer videoId={data.communityVodWithAnnotations.videoId} />
-    {/if}
-    {#if data.communityVodWithAnnotations.service === "Kick"}
-        <KickPlayer videoId={data.communityVodWithAnnotations.videoId} />
-    {/if}
-    {#if data.communityVodWithAnnotations.service === "Youtube"}
-        <YoutubePlayer bind:currentTime bind:duration={videoDuration} bind:seek={setTimestamp} videoId={data.communityVodWithAnnotations.videoId} />
-    {/if}
 </div>
