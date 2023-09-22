4bbea92a31799cb540eea74cd6a2e96e9359ea4a
Ryan <iperish.x@gmail.com>
2023-09-22 13:53:59 +0200

    add global isr

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	preprocess: vitePreprocess(),
-    runtime: "edge",
 	kit: {
 		adapter: adapter(),
         alias: {
