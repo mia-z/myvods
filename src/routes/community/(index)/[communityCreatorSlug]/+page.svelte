00158dca826aae31397e3664f798088f4a1ef472
Ryan <iperish.x@gmail.com>
2023-09-24 11:10:49 +0200

    chore: fix overflowing thumbnails

--- src/routes/community/(index)/[communityCreatorSlug]/+page.svelte
+++ src/routes/community/(index)/[communityCreatorSlug]/+page.svelte
@@ -11,7 +11,7 @@
 
 <div class={"grid grid-cols-3 grid-flow-row gap-3"}>
     {#each data.communityCreatorAndVods.vods as vod}
-        <a href={`/community/${data.communityCreatorAndVods.slug}/${vod.slug}`} class={`card group card-compact col-span-1 image-full bg-base-200 shadow-xl hover:scale-105 active:scale-100 transition-all hover:cursor-pointer select-none ${vod.service === "Youtube" ? "border-red-500 border-2" : vod.service === "Kick" ? "border-2 border-green-200" : vod.service === "Rumble" ? "border-2 border-green-500" : ""}`}>
+        <a href={`/community/${data.communityCreatorAndVods.slug}/${vod.slug}`} class={`card group card-compact col-span-1 image-full bg-base-200 shadow-xl hover:scale-105 active:scale-100 transition-all overflow-hidden hover:cursor-pointer select-none ${vod.service === "Youtube" ? "border-red-500 border-2" : vod.service === "Kick" ? "border-2 border-green-200" : vod.service === "Rumble" ? "border-2 border-green-500" : ""}`}>
             <figure><img class={"min-h-[8rem] max-h-32"} src={vod.videoThumbUrl} alt={"Streamer"} /></figure>
             <div class={"card-body flex flex-col"}>
                 <div class={"text-lg font-bold text-white group-hover:underline line-clamp-2"}>{vod.videoTitle}</div>
